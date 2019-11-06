2599b8119cf5ce6aae15f03c4b9360c2319d4047
i.verizub <iverizub@raraavis.org>
2019-11-06 14:25:27 +0300

    added test login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,12 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from .locators import MainPageLocators
 
+
+def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
+    # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,17 +15,14 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert self.browser.current_url in 'login' "Login link is not found"
+        assert self.browser.current_url, "Login link is not found"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Login email is not presented"
         assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password is not presented"
         assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Registration email is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD), "Registration password is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD2), "Registration password2 is not presented"

82f463390667ee73e1e09fee7f1fe6458b406754
i.verizub <iverizub@raraavis.org>
2019-11-06 16:07:49 +0300

    edit login and main page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,12 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from .locators import MainPageLocators
 
-
-def go_to_login_page(self):
-    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-    link.click()
-    # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 class LoginPage(BasePage):
     def should_be_login_page(self):

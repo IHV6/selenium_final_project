9a2c35238f2ac0f817915434a387e64a7e3a8a8a
i.verizub <iverizub@raraavis.org>
2019-11-11 13:49:35 +0300

    add product page tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -17,7 +17,13 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert total_price == product_price, "Value one is not equal to value two"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), \
+            "Success message is presented, but should not be"
 
+    def should_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), \
+            "Success message is disappeared, but should not be"
 
 
 
